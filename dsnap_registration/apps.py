4978870854dcbfe09b5b8818fc796bfb506a9ff4
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-03-10 11:36:32 -0700

    Start dsnap_registration_service project

---
+++ dsnap_registration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DsnapRegistrationConfig(AppConfig):
+    name = 'dsnap_registration'
---
+++ dsnap_registration/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.postgres.fields import JSONField
+
+class Registration(models.Model):
+    class Meta:
+        db_table = "registration"
+    created_date = models.DateField(null=False, auto_now_add=True)
+    modified_date = models.DateField(null=False, auto_now=True)
+    original_data = JSONField()
+    latest_data = JSONField()
+
+    def __str__(self):
+        return self.original_data['county']
