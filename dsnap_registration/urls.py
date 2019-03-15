8cc3fc2c21ab4571df49c82be9d57d7beb29a02c
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-03-15 14:23:48 -0700

    Switch to generic class-based view

--- dsnap_registration/urls.py
+++ dsnap_registration/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('registrations/', views.registration_list),
+    path('registrations/', views.RegistrationList.as_view()),
 ]
--- dsnap_registration/views.py
+++ dsnap_registration/views.py
@@ -1,22 +1,12 @@
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
+from rest_framework import generics
 
 from .models import Registration
 from .serializers import RegistrationSerializer
 
 
-@csrf_exempt
-def registration_list(request):
-    if request.method == 'GET':
-        registrations = Registration.objects.all()
-        serializer = RegistrationSerializer(registrations, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = RegistrationSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class RegistrationList(generics.ListCreateAPIView):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
