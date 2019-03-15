e78bf3179149a6eed5e0f8d1c3390b84bc3f0562
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-03-15 05:35:23 -0700

    Make original data read-only
    Set to submitted data on `create` and retain value thereafter.

--- dsnap_registration/models.py
+++ dsnap_registration/models.py
@@ -7,7 +7,7 @@ class Registration(models.Model):
         db_table = "registration"
     created_date = models.DateField(null=False, auto_now_add=True)
     modified_date = models.DateField(null=False, auto_now=True)
-    original_data = JSONField()
+    original_data = JSONField(editable=False)
     latest_data = JSONField()
 
     def __str__(self):
--- dsnap_registration/serializers.py
+++ dsnap_registration/serializers.py
@@ -5,3 +5,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = '__all__'
+
+    def create(self, validated_data):
+        """
+        Set original_data (which is set to be not editable) to the latest_data
+        on creation
+        """
+        return Registration.objects.create(original_data=validated_data['latest_data'], **validated_data)
