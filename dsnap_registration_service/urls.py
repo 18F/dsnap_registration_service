00cd874653aff4f8ef1c83897541f7121f25b696
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-04-08 15:06:10 -0700

    Enable admin app

--- dsnap_registration_service/urls.py
+++ dsnap_registration_service/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path(r'', include('dsnap_registration.urls')),
 ]
