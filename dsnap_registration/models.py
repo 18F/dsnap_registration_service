4a9679cc8faf14f5106ddf19a8910482a2906e5f
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-04-08 15:05:46 -0700

    Add modified_by field

--- dsnap_registration/models.py
+++ dsnap_registration/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Registration(models.Model):
     class Meta:
         db_table = "registration"
+    original_data = JSONField(editable=False)
+    latest_data = JSONField()
     created_at = models.DateTimeField(null=False, auto_now_add=True,
                                       editable=False)
     modified_at = models.DateTimeField(null=False, auto_now=True,
                                        editable=False)
-    original_data = JSONField(editable=False)
-    latest_data = JSONField()
+    modified_by = models.ForeignKey('auth.User', null=True,
+                                    related_name='registrations',
+                                    on_delete=models.PROTECT)
