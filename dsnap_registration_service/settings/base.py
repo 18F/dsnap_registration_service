5090162614b354b003875193951fc43703e1c0a9
Adam Biagianti <adam.biagianti@gmail.com>
2019-04-24 17:32:02 -0400

    Specify bsic auth in api settings

--- dsnap_registration_service/settings/base.py
+++ dsnap_registration_service/settings/base.py
@@ -136,4 +136,7 @@ REST_FRAMEWORK = {
     ),
     'NON_FIELD_ERRORS_KEY': 'Invalid request',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    )
 }
